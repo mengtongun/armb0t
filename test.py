55b3ea0a83a5acab97ca8fd6aa3aac2b0f504336
kimhak <hengkimhak18@kit.edu.kh>
2020-12-25 15:38:53 +0700

    Second Edit

--- test.py
+++ test.py
@@ -4,19 +4,9 @@
 # import the necessary packages
 from collections import deque
 import numpy as np
-import argparse
-import imutils
 import cv2
-import urllib #for reading image from URL
- 
- 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video",
-    help="path to the (optional) video file")
-ap.add_argument("-b", "--buffer", type=int, default=64,
-    help="max buffer size")
-args = vars(ap.parse_args())
+#import urllib #for reading image from URL
+
  
 # define the lower and upper boundaries of the colors in the HSV color space
 lower = {'red':(166, 84, 141), 'green':(66, 122, 129), 'blue':(97, 100, 117), 'yellow':(23, 59, 119), 'orange':(0, 50, 80)} #assign new item lower['blue'] = (93, 10, 0)
@@ -25,25 +15,13 @@ upper = {'red':(186,255,255), 'green':(86,255,255), 'blue':(117,255,255), 'yello
 # define standard colors for circle around the object
 colors = {'red':(0,0,255), 'green':(0,255,0), 'blue':(255,0,0), 'yellow':(0, 255, 217), 'orange':(0,140,255)}
  
-#pts = deque(maxlen=args["buffer"])
- 
-# if a video path was not supplied, grab the reference
-# to the webcam
-if not args.get("video", False):
-    camera = cv2.VideoCapture(2)
-    
- 
-# otherwise, grab a reference to the video file
-else:
-    camera = cv2.VideoCapture(args["video"])
+camera = cv2.VideoCapture(2)
+
 # keep looping
 while True:
     # grab the current frame
     (grabbed, frame) = camera.read()
-    # if we are viewing a video and we did not grab a frame,
-    # then we have reached the end of the video
-    if args.get("video") and not grabbed:
-        break
+    frame = cv2.flip(frame, 0)
  
     #IP webcam image stream 
     #URL = 'http://10.254.254.102:8080/shot.jpg'
@@ -51,10 +29,7 @@ while True:
     #frame = cv2.imread('shot1.jpg')
  
  
-    # resize the frame, blur it, and convert it to the HSV
-    # color space
-    frame = imutils.resize(frame, width=600)
- 
+    # blur the frame, and convert it to the HSV
     blurred = cv2.GaussianBlur(frame, (11, 11), 0)
     hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
     #for each color in dictionary check object in frame
@@ -82,6 +57,7 @@ while True:
             ((x, y), radius) = cv2.minEnclosingCircle(c)
             M = cv2.moments(c)
             center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+            print(center[0], center[1], key)
         
             # only proceed if the radius meets a minimum size. Correct this value for your obect's size
             if radius > 0.5:
